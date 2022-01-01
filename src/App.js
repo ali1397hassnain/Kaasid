986f76fe3fa5e6b400630411e62a516ddfaff49c
Ali Hassnain <ali1397hasnain@gmail.com>
2022-01-01 14:31:36 +0500

    changed to functional components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 import './App.css';
-import React, { Component } from 'react'
+
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import LoadingBar from 'react-top-loading-bar' 
-export default class App extends Component {
-  state={
-    progress:10
-  }
-  setProgress = (progress) => {
-    this.setState({progress})
-  }
-  render() {
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App = ()=> {
+  const pageSize = 5;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0)
+ 
     return (
       <div>
         <Router>
-        <Navbar/>
-        <LoadingBar 
+        <Navbar/> 
+        <LoadingBar
+        height={3}
         color='#f11946'
-        progress={this.state.progress}
-        />
+        progress={progress} 
+      />
         <Switch>
-          <Route exact path="/business"><News setProgress={this.setProgress}  key="business" PageSize={8} country="gb" category="business" /></Route>
-          <Route exact path="/health"><News setProgress={this.setProgress}  key="health" PageSize={8} country="gb" category="health" /></Route>
-          <Route exact path="/sports"><News setProgress={this.setProgress}  key="sports" PageSize={8} country="gb" category="sports" /></Route>
-          <Route exact path="/technology"><News setProgress={this.setProgress}  key="technology" PageSize={8} country="gb" category="technology" /></Route>
-          <Route exact path="/"><News setProgress={this.setProgress}  key="general" PageSize={8} country="gb" category="general" /></Route>
-          <Route exact path="/general"><News setProgress={this.setProgress}  key="general" PageSize={8} country="gb" category="general" /></Route>
-          <Route exact path="/science"><News setProgress={this.setProgress}  key="science" PageSize={8} country="gb" category="science" /></Route>
-          <Route exact path="/entertainment"><News setProgress={this.setProgress}  key="entertainment" PageSize={8} country="gb" category="entertainment" /></Route>
+          <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="usa" category="general"/></Route> 
+          <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="usa" category="business"/></Route> 
+          <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="usa" category="entertainment"/></Route> 
+          <Route exact path="/general"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="usa" category="general"/></Route> 
+          <Route exact path="/health"><News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="usa" category="health"/></Route> 
+          <Route exact path="/science"><News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="usa" category="science"/></Route> 
+          <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="usa" category="sports"/></Route> 
+          <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="usa" category="technology"/></Route> 
         </Switch>
         </Router>
       </div>
     )
-  }
+ 
 }
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from "react-router-dom";
 
-export class Navbar extends Component {
+const Navbar = () => {
 
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div className="container">
-                        <Link className="navbar-brand" to="/">Kaasid</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        </button>
-                        <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+    return (
+        <div>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Kaasid</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link" aria-current="page" to="/">Home</Link>
@@ -23,14 +23,13 @@ export class Navbar extends Component {
                             <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-
-
                         </ul>
-                        </div>
                     </div>
-                    </nav>
-            </div>
-        )
-    }
+                </div>
+            </nav>
+        </div>
+    )
+
 }
+
 export default Navbar
